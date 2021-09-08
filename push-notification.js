82c6ee8a8f007b925314dca5fd4192e84d10ca43
mash <moshiour0027@gmail.com>
2021-09-09 00:57:56 +0600

    updated

--- push-notification.js
+++ push-notification.js
@@ -6,28 +6,37 @@ let ErrorCount = new Counter("errors");
 let ErrorRate = new Rate("error_rate");
 
 export let options = {
+  //   stages: [
+  //     { duration: "5m", target: 60 }, // simulate ramp-up of traffic from 1 to 60 users over 5 minutes.
+  //     { duration: "10m", target: 60 }, // stay at 60 users for 10 minutes
+  //     { duration: "3m", target: 100 }, // ramp-up to 100 users over 3 minutes (peak hour starts)
+  //     { duration: "2m", target: 100 }, // stay at 100 users for short amount of time (peak hour)
+  //     { duration: "3m", target: 60 }, // ramp-down to 60 users over 3 minutes (peak hour ends)
+  //     { duration: "10m", target: 60 }, // continue at 60 for additional 10 minutes
+  //     { duration: "5m", target: 0 }, // ramp-down to 0 users
+  //   ],
   stages: [
-    { duration: "5m", target: 60 }, // simulate ramp-up of traffic from 1 to 60 users over 5 minutes.
-    { duration: "10m", target: 60 }, // stay at 60 users for 10 minutes
-    { duration: "3m", target: 100 }, // ramp-up to 100 users over 3 minutes (peak hour starts)
-    { duration: "2m", target: 100 }, // stay at 100 users for short amount of time (peak hour)
-    { duration: "3m", target: 60 }, // ramp-down to 60 users over 3 minutes (peak hour ends)
-    { duration: "10m", target: 60 }, // continue at 60 for additional 10 minutes
+    { duration: "1m", target: 100 }, // simulate ramp-up of traffic from 1 to 60 users over 5 minutes.
+    { duration: "3m", target: 200 }, // stay at 60 users for 10 minutes
+    { duration: "3m", target: 300 }, // ramp-up to 100 users over 3 minutes (peak hour starts)
+    { duration: "7m", target: 350 }, // stay at 100 users for short amount of time (peak hour)
+    { duration: "7m", target: 200 }, // ramp-down to 60 users over 3 minutes (peak hour ends)
+    { duration: "7m", target: 200 }, // continue at 60 for additional 10 minutes
     { duration: "5m", target: 0 }, // ramp-down to 0 users
   ],
-  //   noConnectionReuse: true,
+  noConnectionReuse: true,
   //   thresholds: {
   //     http_req_duration: ["p(99)<1500"], // 99% of requests must complete below 1.5s
   //   },
 };
 
 export function setup() {
-  const url = "https://oceannow.ap.ocean.com/api/loginWithReservation";
+  const url = "https://oceannow.xs.ocean.com/api/loginWithReservation";
   let data = {
-    firstName: "Alexander",
-    lastName: "Rosa",
-    reservationNumber: "2V6DWM",
-    birthDate: "1992-09-14",
+    firstName: "CHRISTOPHER",
+    lastName: "HASLUCK",
+    reservationNumber: "2J5KVG",
+    birthDate: "1947-07-02",
   };
 
   let res = http.post(url, JSON.stringify(data), {
@@ -45,10 +54,10 @@ export function setup() {
 }
 
 export default function (authToken) {
-  const url = "https://oceannow.ap.ocean.com/api/sendCustomPushMessage";
+  const url = "https://oceannow.xs.ocean.com/api/sendCustomPushMessage";
 
   let data = {
-    userid: "ocean-6ded1097-54fa-4cc2-b53b-c91c64b728c3",
+    userid: "ocean-52284127-844f-4ef5-bcdf-5f305641e573",
     message: "Can you see this",
     title: "Hello",
     data: {
