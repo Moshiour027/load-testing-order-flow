2a23e5fa61f9869586c4b00e343dab5b7caa1666
mash <moshiour0027@gmail.com>
2021-09-10 14:36:57 +0600

    finished push-notification with env

--- push-notification.js
+++ push-notification.js
@@ -6,24 +6,12 @@ let ErrorCount = new Counter("errors");
 let ErrorRate = new Rate("error_rate");
 
 export let options = {
-  //   stages: [
-  //     { duration: "5m", target: 60 }, // simulate ramp-up of traffic from 1 to 60 users over 5 minutes.
-  //     { duration: "10m", target: 60 }, // stay at 60 users for 10 minutes
-  //     { duration: "3m", target: 100 }, // ramp-up to 100 users over 3 minutes (peak hour starts)
-  //     { duration: "2m", target: 100 }, // stay at 100 users for short amount of time (peak hour)
-  //     { duration: "3m", target: 60 }, // ramp-down to 60 users over 3 minutes (peak hour ends)
-  //     { duration: "10m", target: 60 }, // continue at 60 for additional 10 minutes
-  //     { duration: "5m", target: 0 }, // ramp-down to 0 users
-  //   ],
   stages: [
-    { duration: "1m", target: 100 }, // simulate ramp-up of traffic from 1 to 60 users over 5 minutes.
-    { duration: "3m", target: 200 }, // stay at 60 users for 10 minutes
-    { duration: "3m", target: 300 }, // ramp-up to 100 users over 3 minutes (peak hour starts)
-    { duration: "7m", target: 350 }, // stay at 100 users for short amount of time (peak hour)
-    { duration: "7m", target: 200 }, // ramp-down to 60 users over 3 minutes (peak hour ends)
-    { duration: "7m", target: 200 }, // continue at 60 for additional 10 minutes
-    { duration: "5m", target: 0 }, // ramp-down to 0 users
+    // { duration: "1m", target: 25 },
+    // { duration: "1m", target: 40 },
+    // 1020 request were made in 2 minutes
   ],
+
   noConnectionReuse: true,
   //   thresholds: {
   //     http_req_duration: ["p(99)<1500"], // 99% of requests must complete below 1.5s
@@ -31,12 +19,12 @@ export let options = {
 };
 
 export function setup() {
-  const url = "https://oceannow.xs.ocean.com/api/loginWithReservation";
+  const url = `https://oceannow.${__ENV.ENV}.ocean.com/api/loginWithReservation`;
   let data = {
-    firstName: "CHRISTOPHER",
-    lastName: "HASLUCK",
-    reservationNumber: "2J5KVG",
-    birthDate: "1947-07-02",
+    firstName: "MILES",
+    lastName: "CLARK",
+    reservationNumber: "2VXH2J",
+    birthDate: "1993-07-07",
   };
 
   let res = http.post(url, JSON.stringify(data), {
@@ -48,16 +36,17 @@ export function setup() {
     ErrorCount.add(1);
     ErrorRate.add(1);
   } else {
-    let authToken = res.json("authToken");
-    return authToken;
+    const authToken = res.json("authToken");
+    const userId = res.json("userId");
+    return { authToken, userId };
   }
 }
 
-export default function (authToken) {
-  const url = "https://oceannow.xs.ocean.com/api/sendCustomPushMessage";
+export default function ({ authToken, userId }) {
+  const url = `https://oceannow.${__ENV.ENV}.ocean.com/api/sendCustomPushMessage`;
 
   let data = {
-    userid: "ocean-52284127-844f-4ef5-bcdf-5f305641e573",
+    userid: userId,
     message: "Can you see this",
     title: "Hello",
     data: {
